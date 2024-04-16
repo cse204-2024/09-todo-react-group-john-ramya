9d4f888ab12c76c130373559c36cbab444d22339
samudraam <112435495+samudraam@users.noreply.github.com>
2024-04-15 22:09:08 -0500

    Background change
    Changing bg color

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Todo from "./Todo";
-import NewTodo from "./NewTodo";
 
 function App() {
   return (
     <div className="App">
       <header>
-        <h1>John and Ramya's To Do Items</h1>
+        <h1>To Do Items</h1>
       </header>
       <section id="todo">
         <div id="alltodo" className="todo-items">
